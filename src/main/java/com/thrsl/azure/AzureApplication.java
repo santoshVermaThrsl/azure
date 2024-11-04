c7f78c766d150c73c0387947bbb290e70779ade5
santoshVermaThrsl <santosh.kumar@hitechroboticsystemz.com>
2024-11-04 15:08:13 +0530

    second commit

--- src/main/java/com/thrsl/azure/AzureApplication.java
+++ src/main/java/com/thrsl/azure/AzureApplication.java
@@ -11,6 +11,10 @@ public class AzureApplication {
 	public String data(){
 		return "This is my first application on azure !";
 	}
+	@GetMapping("/")
+	public String data1(){
+		return "This is my first application on azure1 !";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureApplication.class, args);
